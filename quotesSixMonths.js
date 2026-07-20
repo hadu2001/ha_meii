6add69a3a4752793b81f7461ecadc607f2b4b56c
HA <sonbautroi1999@gmail.com>
2026-07-20 22:14:49 +0700

    add

--- quotesSixMonths.js
+++ quotesSixMonths.js
@@ -21,16 +21,16 @@ const sixMonthsQuotes = [
 ];
 
 const sixMonthsBackgrounds = [
-  "./assets/anh_6_month/IMG_1.jpg",
-  "./assets/anh_6_month/IMG_20260111_235057_080.jpg",
-  "./assets/anh_6_month/IMG_20260112_000822_039.jpg",
-  "./assets/anh_6_month/album_MHW0075G7Pdr_output.jpg",
   "./assets/anh_6_month/z8035702974025_bcb15eddee0e530726b30a6925d6ac4c.jpg",
   "./assets/anh_6_month/z8035702980404_064d1287efc371d791e48f6a1ee426b8.jpg",
   "./assets/anh_6_month/z8035705303309_c4d3686e72b54b78802b5fbdd467a4eb.jpg",
   "./assets/anh_6_month/z8035705303420_1478876f6488cfc055a548a5f665921a.jpg",
   "./assets/anh_6_month/z8035725445396_41c70fefc64913c90fd60f9b91a10f55.jpg",
-  "./assets/anh_6_month/z8035725452100_df07e292e9a6dc6caa2b5c87bff89a8f.jpg"
+  "./assets/anh_6_month/z8035725452100_df07e292e9a6dc6caa2b5c87bff89a8f.jpg",
+  "./assets/anh_6_month/z8061078512638_df9509dec8c6f6b5ff8116734a2f1263.jpg",
+  "./assets/anh_6_month/z8061078556696_7cb104d8521f427eaf21e06045843443.jpg",
+  "./assets/anh_6_month/z8063982733442_324e6c2c65c9bb8d5681cdf5fcb4736c.jpg",
+  "./assets/anh_6_month/z8063984245424_6e6648be2d6c2e02ab84403bf8780d08.jpg"
 ];
 
 // Render quotes
